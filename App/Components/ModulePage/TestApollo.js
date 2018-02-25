bb1ae8a279c3ef5a4c2c789c63722497b14d87f2
Unknown <xemnaskami@gmail.com>
2018-02-25 16:57:02 +0000

    First request on Api

--- App/Components/ModulePage/TestApollo.js
+++ App/Components/ModulePage/TestApollo.js
@@ -2,7 +2,35 @@ import React  from 'react';
 
 import { Container, Content, Text } from 'native-base';
 
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
+
+
 export default class TestApollo extends React.Component {
+
+    Data;
+
+    constructor(...args) {
+        super(...args);
+
+        const client = new ApolloClient({
+            link: new HttpLink({ uri: 'http://lferry.xyz:8080/graphql' }),
+            cache: new InMemoryCache()
+        });
+
+        client.query({
+            query: gql`
+                query getAllModules {
+                    allModules {
+                        name: id, type, location, id
+                    }
+                }
+            `,
+        }).then(data => console.log(data))
+            .catch(error => console.log(error))
+    }
 
     render () {
         return (
